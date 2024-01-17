c0b4ca62aa39ad5d0920cb33d50d90f7d60f4ee5
ercrrc <134560333+ercrrc@users.noreply.github.com>
2024-01-17 18:55:35 -0300

    projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import Vue from 'vue';
+import App from './App';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+Vue.config.productionTip = false;
 
-createApp(App).mount('#app')
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App/>',
+});
